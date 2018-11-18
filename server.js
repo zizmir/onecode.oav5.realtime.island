47f381a3f856d15c14f252b6c44cd9e9ae757a23
m'bark <mbark.bouznir@gmail.com>
2018-11-18 10:28:43 +0100

    base magicNumber

--- server.js
+++ server.js
@@ -1,54 +1,75 @@
-import express from 'express'
-import socketIo from 'socket.io'
-import path from 'path'
-
-import { createServer } from 'http'
-
-import { argv, mlog } from './libs/utils'
+import express from "express";
+import socketIo from "socket.io";
+import path from "path";
+import { createServer } from "http";
+import fs from "fs";
+import { argv, mlog } from "./libs/utils";
 
 // Instantiate express application
-const app = express()
+const app = express();
 
 // Setting the application port depending to environment
-const port = parseInt(argv[0], 10) || process.env.PORT
+const port = parseInt(argv[0], 10) || process.env.PORT;
+
+// magicNumber
+let magicNumber = Math.floor(Math.random() * 1337);
 
 // Entry point function
 const start = async () => {
   try {
     // app is a request handler function that we must pass to http server instance
-    const server = createServer(app)
+    const server = createServer(app);
 
     // socket.io take a server and not an express application
-    const io = socketIo(server)
+    const io = socketIo(server);
 
     // ... and finally server listening not the app
-    server.listen(port, (err) => {
-      if (err) throw err
+    server.listen(port, err => {
+      if (err) throw err;
 
-      mlog(`Server is running on port ${port}`)
-    })
+      mlog(`Server is running on port ${port}`);
+    });
+    let countUserConnect = 0;
 
-    io.on('connection', (socket) => {
-      mlog('client connected', 'yellow')
+    io.on("connection", socket => {
+      countUserConnect++;
+      mlog("client connected", "yellow");
 
-      socket.on('disconnect', () => {
-        mlog('client disconnected', 'yellow')
-      })
+      socket.on("disconnect", () => {
+        countUserConnect--;
 
-      socket.on('join', (nickname) => {
-        console.log(`${nickname} has joined the channel`)
-        io.emit('hello', 'Welcome to the jungle!')
-      })
+        mlog("client disconnected", "yellow");
+      });
 
-      socket.on('message', (content) => {
-        socket.broadcast.emit('newMessage', content)
-      })
-    })
+      socket.on("join", nickname => {
+        console.log(`${nickname} has joined the channel`);
+
+        io.emit("hello", "Welcome to the jungle!");
+      });
+
+      socket.on("message", content => {
+        socket.broadcast.emit("newMessage", content);
+      });
+      app.get("/magicNumber", (req, res) => {
+        socket.on("try", value => {
+          if (magicNumber == value) {
+            io.emit("response", "You win !!");
+            socket.broadcast.emit("response", " You looooooose !");
+            if (!fs.existsSync("./games.json")) {
+            }
+          } else if (magicNumber < value) {
+            io.emit("response", "to upper");
+          } else if (magicNumber > value) {
+            io.emit("response", "to lower");
+          }
+        });
+      });
+    });
   } catch (err) {
-    mlog(err, 'red')
-    process.exit(42)
+    mlog(err, "red");
+    process.exit(42);
   }
-}
+};
 
 // Let's Rock!
-start()
+start();

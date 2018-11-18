5bf750134e35db438ba0f75f0f22dcdd0ba120f5
m'bark <mbark.bouznir@gmail.com>
2018-11-18 14:07:13 +0100

    add magicNumber without file gamejson check

--- server.js
+++ server.js
@@ -4,10 +4,14 @@ import path from "path";
 import { createServer } from "http";
 import fs from "fs";
 import { argv, mlog } from "./libs/utils";
+import { constMN } from "./constants/constGames";
 
 // Instantiate express application
 const app = express();
 
+let countUserInGames = {
+  COUNT_GAMERS_MAGIC_NUMBER: 0
+};
 // Setting the application port depending to environment
 const port = parseInt(argv[0], 10) || process.env.PORT;
 
@@ -29,40 +33,88 @@ const start = async () => {
 
       mlog(`Server is running on port ${port}`);
     });
-    let countUserConnect = 0;
 
     io.on("connection", socket => {
-      countUserConnect++;
       mlog("client connected", "yellow");
 
       socket.on("disconnect", () => {
-        countUserConnect--;
-
         mlog("client disconnected", "yellow");
       });
 
       socket.on("join", nickname => {
         console.log(`${nickname} has joined the channel`);
-
         io.emit("hello", "Welcome to the jungle!");
       });
 
       socket.on("message", content => {
         socket.broadcast.emit("newMessage", content);
       });
-      app.get("/magicNumber", (req, res) => {
-        socket.on("try", value => {
+    });
+
+    // GAME MAGIC NUMBER
+    let ioMagicNumber = io.of("/magicNumber");
+    // OPEN GAME
+    ioMagicNumber.on("connection", socket => {
+      //user join the game
+      socket.on("join", nickname => {
+        console.log(nickname);
+        // increment number of user connected
+        countUserInGames.COUNT_GAMERS_MAGIC_NUMBER++;
+        // saved the name of user
+        socket.nickname = nickname;
+
+        socket.emit(
+          "boot",
+          `[BOOT] Welcome ${nickname}  ! In the game : Magic Number`
+        );
+        socket.broadcast.emit(
+          "boot",
+          `[BOOT] ${socket.nickname} has joined the game! `
+        );
+      });
+
+      socket.on("try", value => {
+        console.log(countUserInGames.COUNT_GAMERS_MAGIC_NUMBER);
+        // change the number 1 by const ""< MIN_USER_AUTHORIZED_IN_ROOM" if your are not alone
+        if (countUserInGames.COUNT_GAMERS_MAGIC_NUMBER >= 1) {
+          // if number is higher of min user connect send a message for user connect and the other
+          console.log(countUserInGames.COUNT_GAMERS_MAGIC_NUMBER);
+          console.log(socket.nickname);
+
+          socket.broadcast.emit("response", "The game started !");
+          ioMagicNumber.emit(
+            "boot",
+            `the magic number is ${magicNumber} ${value}`
+          );
+
           if (magicNumber == value) {
-            io.emit("response", "You win !!");
-            socket.broadcast.emit("response", " You looooooose !");
-            if (!fs.existsSync("./games.json")) {
+            ioMagicNumber.emit("response", "😎 You win !!");
+            socket.broadcast.emit(
+              "boot",
+              `😠 Damn! the gamers ${socket.nickname} find the number `
+            );
+            if (fs.existsSync("./games.json")) {
+              ioMagicNumber.emit("exist", true);
+            } else {
+              ioMagicNumber.emit("exist", false);
             }
-          } else if (magicNumber < value) {
-            io.emit("response", "to upper");
-          } else if (magicNumber > value) {
-            io.emit("response", "to lower");
           }
-        });
+          if (magicNumber < value) {
+            ioMagicNumber.emit("boot", "😱 To upper");
+          }
+          if (magicNumber > value) {
+            ioMagicNumber.emit("boot", "😰 To lower");
+          }
+        } else {
+          console.log("waiting challenger");
+          ioMagicNumber.emit("boot", "Waiting challenger ....");
+        }
+      });
+
+      socket.on("disconnect", () => {
+        countUserInGames.COUNT_GAMERS_MAGIC_NUMBER--;
+
+        mlog("client disconnected", "yellow");
       });
     });
   } catch (err) {
